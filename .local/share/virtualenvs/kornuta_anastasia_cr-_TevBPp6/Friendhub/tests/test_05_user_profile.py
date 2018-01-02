4832095a41ae18a4a5f0a86cd39d941a188ee1e2
Primula13 <kornutaa@gmail.com>
2018-01-02 15:28:11 +0200

    Update test_05_user_profile.py

--- .local/share/virtualenvs/kornuta_anastasia_cr-_TevBPp6/Friendhub/tests/test_05_user_profile.py
+++ .local/share/virtualenvs/kornuta_anastasia_cr-_TevBPp6/Friendhub/tests/test_05_user_profile.py
@@ -4,46 +4,37 @@ from Friendhub.configurations import models
 from Friendhub.tools import user_tools
 
 uri = "http://friendhub.dev.cleveroad.com/admin/api/v1/"
-pass_list = [("qwerty1", "qwerty1"), ("qwer1", "a123123123"), ("oankBJfFcnatbB9JLM1VMnQFR8LhmIpUhswuQpU9L",
-             "qwerty1"), ("qwerty", "qwerty1"), ("123456", "qwerty1"), ("qwerty1", " qwerty1")]
 token = models.user1.token
+password = models.user1.password
 
-def test_01_user_change_pass_too_short():
-    response = user_tools.change_pass_user(pass_list[1][0], pass_list[1][1])
-    assert response[0] == 400
-    assert response[1] == "Password must contain at least 6 characters."
 
-def test_02_user_change_pass_too_long():
-    response = user_tools.change_pass_user(pass_list[2][0], pass_list[2][1])
-    assert response[0] == 400
-    assert response[1] == "is too long (maximum is 40 characters)"
+def test_01_user_change_password_invalid_new_password(new_passwords_invalid):
+    new_password = new_passwords_invalid["new_password"]
+    response = user_tools.change_pass_user(password, new_password)
+    assert response[0] == new_passwords_invalid["expected_code"]
+    assert response[1] == new_passwords_invalid["expected_message"]
 
-def test_03_user_change_pass_digitless():
-    response = user_tools.change_pass_user(pass_list[3][0], pass_list[3][1])
-    assert response[0] == 400
-    assert response[1] == "Password must include both numbers and letters."
 
-def test_04_user_change_pass_only_digits():
-    response = user_tools.change_pass_user(pass_list[4][0], pass_list[4][1])
-    assert response[0] == 400
-    assert response[1] == "Password must include both numbers and letters."
-
-def test_05_user_change_pass_success():
-    response = user_tools.change_pass_user(pass_list[0][0], pass_list[0][1])
+def test_02_user_change_password_success():
+    new_password = "qwerty11"
+    response = user_tools.change_pass_user(new_password, password)
     assert response[0] == 200
-    assert user_tools.user_auth(models.user1.email, pass_list[0][0]) == 200
+    assert user_tools.user_auth(models.user1.email, new_password) == 200
+    user_tools.change_pass_user(password, new_password)
 
-def test_06_user_change_pass_wrongpass():
+
+def test_03_user_change_password_wrong_old_password():
     auth_header = {"Authorization": token}
-    payload = {"password": pass_list[5][0], "oldPassword": pass_list[5][1]}
+    payload = {"password": password, "oldPassword": " " + password}
     r = requests.patch(url=uri + 'users/password', headers=auth_header, data=payload)
     assert r.status_code == 400
     assert r.json()["message"] == "The password you entered is incorrect. Please try again."
 
-def test_07_user_change_pass_permission_admin_token():
+
+def test_04_user_change_pass_permission_admin_token():
     admin_token = models.admin.token
     auth_header = {"Authorization": admin_token}
-    payload = {"password": pass_list[0][0], "oldPassword": pass_list[0][1]}
+    payload = {"password": password, "oldPassword": password}
     r = requests.patch(url=uri + 'users/password', headers=auth_header, data=payload)
     assert r.status_code == 401
     assert r.json()["message"] == "You have provided invalid token!"
